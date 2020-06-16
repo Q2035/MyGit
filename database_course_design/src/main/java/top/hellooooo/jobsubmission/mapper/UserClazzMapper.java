3b397aa073449e460d0177e4eb20f7ec8b42bde4
Q2035 <2383862181@qq.com>
2020-06-17 07:20:53 +0800

    add checkbox of clazz

---
+++ database_course_design/src/main/java/top/hellooooo/jobsubmission/mapper/UserClazzMapper.java
@@ -0,0 +1,9 @@
+package top.hellooooo.jobsubmission.mapper;
+
+import top.hellooooo.jobsubmission.pojo.Clazz;
+
+import java.util.List;
+
+public interface UserClazzMapper {
+    public List<Clazz> getAllClazz();
+}
--- database_course_design/src/main/java/top/hellooooo/jobsubmission/pojo/Clazz.java
+++ database_course_design/src/main/java/top/hellooooo/jobsubmission/pojo/Clazz.java
@@ -3,4 +3,20 @@ package top.hellooooo.jobsubmission.pojo;
 public class Clazz {
     private Integer id;
     private String className;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 }
