227451f6c9dedb4e0c52a12d7b2bebfc3825214f
Q2035 <2383862181@qq.com>
2020-07-01 20:59:09 +0800

    prepare for network course design

--- Network_course_design/src/main/java/top/hellooooo/jobsubmission/util/AccountStatus.java
+++
@@ -1,6 +0,0 @@
-package main.java.top.hellooooo.jobsubmission.util;
-
-public class AccountStatus {
-    public static final Integer UNAUTHORIZED_ACCESS_TO = 1;
-    public static final Integer TOO_MANY_PASSWORD_ERRORS = 2;
-}
